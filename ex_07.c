039bca7de6db05b863d088981e908a87f77b2948
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-06-05 14:12:39 -0300

    007

---
+++ ex_07.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+typedef struct {
+  double x;
+  double y;
+} Ponto;
+
+typedef struct {
+  double x;
+  double y;
+  double largura;
+  double altura;
+} Retangulo;
+
+Ponto lerPonto() {
+  Ponto p;
+  scanf("%lf %lf", &p.x, &p.y);
+  return p;
+}
+
+Retangulo lerRetangulo(int numero) {
+  Retangulo r;
+  scanf("%lf", &r.x);
+  scanf("%lf", &r.y);
+  scanf("%lf", &r.largura);
+  scanf("%lf", &r.altura);
+  return r;
+}
+
+int pontoNoRetangulo(Ponto p, Retangulo r) {
+  return (
+    p.x >= r.x &&
+    p.x <= r.x + r.largura &&
+    p.y >= r.y &&
+    p.y <= r.y + r.altura
+  );
+}
+
+Retangulo criarRetangulo(double x, double y, double largura, double altura) {
+  Retangulo r;
+  r.x = x;
+  r.y = y;
+  r.largura = largura;
+  r.altura = altura;
+  return r;
+}
+
+Ponto criarPonto(double x, double y) {
+  Ponto p;
+  p.x = x;
+  p.y = y;
+  return p;
+}
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
